591e47b911624e2d5a4a49610cfa73d94e668b1c
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2020-03-21 04:53:27 +0000

    add proximity based voice

--- client.lua
+++ client.lua
@@ -6,6 +6,9 @@ local voiceModes = {
 }
 local voiceMode = 2
 
+local targetChunks = {}
+local lastChunks = {}
+
 RegisterCommand("+voiceMode", function()
 	local newMode = voiceMode + 1
 
@@ -37,41 +40,15 @@ AddEventHandler("mumble:SetVoiceData", function(data)
 	voiceData = data
 end)
 
-
 Citizen.CreateThread(function()
-	local headBone = 0x796e
 	while true do
 		Citizen.Wait(0)
 		local playerId = PlayerId()
-		local playerPed = PlayerPedId()
-		local playerHeading = math.rad(GetGameplayCamRot().z % 360)
-		local playerPos = GetPedBoneCoords(playerPed, headBone)
-		local playerList = GetActivePlayers()
 
-		for i = 1, #playerList do
-			local remotePlayerId = playerList[i]
-
-			if playerId ~= remotePlayerId then
-				local remotePlayerServerId = GetPlayerServerId(remotePlayerId)
-				local remotePlayerPed = GetPlayerPed(remotePlayerId)
-				local remotePlayerPos = GetPedBoneCoords(remotePlayerPed, headBone)
-				local remotePlayerData = voiceData[remotePlayerServerId]
-
-				local distance = #(playerPos - remotePlayerPos)
-				local mode = 2
-
-				if remotePlayerData ~= nil then
-					mode = remotePlayerData.mode or 2
-				end
 
 				--print("player:" .. remotePlayerServerId, "distance: " .. distance, "mode:" .. voiceModes[mode][1], "volume:" .. volume)
 
-				if distance < voiceModes[mode][1] then
-					local volume = 1.0 - (distance / voiceModes[mode][1])
 
-					if volume < 0 then
-						volume = 0.0
-					end
 
 					MumbleSetVolumeOverride(remotePlayerId, volume)
 				else
@@ -105,9 +82,6 @@ function GetChunkChannel(v)
     return (v.x << 8) | v.y
 end
 
-local targetChunks = {}
-local lastChunks = {}
-
 -- loop
 --neptunium
 Citizen.CreateThread(function()
@@ -119,7 +93,7 @@ Citizen.CreateThread(function()
 		local playerPos = GetPedBoneCoords(playerPed, headBone)
 		local playerList = GetActivePlayers()
 
-		local currentChunk = vector2(GetGridChunk(coords.x), GetGridChunk(coords.y)) -- Chunk player is in
+		local currentChunk = vector2(GetGridChunk(playerPos.x), GetGridChunk(playerPos.y)) -- Chunk player is in
 		local chunkChannel = GetChunkChannel(currentChunk) -- Get voice channel for chunk
 
 		NetworkSetVoiceChannel(chunkChannel) -- Set voice channel
@@ -127,11 +101,11 @@ Citizen.CreateThread(function()
 		targetChunks = {} -- Clear list of target chunks
 
 		for i = 1, #deltas do -- Get nearby chunks
-			local chunkSize = coords.xy + (deltas[i] * 20) -- edge size
+			local chunkSize = playerPos.xy + (deltas[i] * 20) -- edge size
 			local chunk = vector2(GetGridChunk(chunkSize.x), GetGridChunk(chunkSize.y)) -- get nearby chunk
 			local channel = GetChunkChannel(chunk) -- Get voice channel for chunk
 
-			targetList[channel] = true -- add chunk to target list
+			targetChunks[channel] = true -- add chunk to target list
 		end
 		
 		-- super naive hash difference
@@ -164,6 +138,38 @@ Citizen.CreateThread(function()
 			MumbleSetVoiceTarget(2) -- Broadcast voice to target
 
 			lastChunks = targetChunks -- Store chunks list
+		end
+
+		for i = 1, #playerList do -- Proximity based voice (probably won't work for infinity? near a grid border?)
+			local remotePlayerId = playerList[i]
+
+			if playerId ~= remotePlayerId then
+				local remotePlayerServerId = GetPlayerServerId(remotePlayerId)
+				local remotePlayerPed = GetPlayerPed(remotePlayerId)
+				local remotePlayerPos = GetPedBoneCoords(remotePlayerPed, headBone)
+				local remotePlayerData = voiceData[remotePlayerServerId]
+
+				local distance = #(playerPos - remotePlayerPos)
+				local mode = 2
+
+				if remotePlayerData ~= nil then
+					mode = remotePlayerData.mode or 2
+				end
+
+				if distance < voiceModes[mode][1] then
+					local volume = 1.0 - (distance / voiceModes[mode][1])^0.5
+
+					if volume < 0 then
+						volume = 0.0
+					end
+
+					print("player:" .. remotePlayerServerId, "distance: " .. distance, "mode:" .. voiceModes[mode][1], "volume:" .. volume)
+
+					MumbleSetVolumeOverride(remotePlayerId, volume)
+				else
+					MumbleSetVolumeOverride(remotePlayerId, 0.0)
+				end
+			end
 		end
 	end
 end)
