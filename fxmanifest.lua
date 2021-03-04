3582cb7fe3a0feb943b43bdaab6f6efe0dd56098
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-03-04 10:59:16 +0000

    init overhaul

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+games { "gta" }
+
+author "Fraser Watt (https://github.com/FrazzIe)"
+description "A manager for FiveM's implementation of mumble-voip"
+version "1.6"
+
+lua54 "yes"
+
+ui_page ""
+files {}
+
+client_scripts {}
+server_scripts {}
