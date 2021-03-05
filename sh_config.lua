ad8c366fb8f90578c5a58accc6afa8158e47a5be
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-03-05 14:44:54 +0000

    add proximity modes

--- sh_config.lua
+++ sh_config.lua
@@ -1,3 +1,8 @@
 config = {
 	debug = true,
+	proximity = {
+		{ name = "whisper", input = 3.0, output = 2.5 },
+		{ name = "normal", input = 3.0, output = 8.0 },
+		{ name = "shouting", input = 3.0, output = 20.0 },
+	}
 }
