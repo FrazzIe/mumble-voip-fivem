c41389588d69d7a1c48654f9b7accfa9b951d0e5
Fraser Watt <28698343+FrazzIe@users.noreply.github.com>
2021-04-20 13:07:34 +0100

    move proximity natives into proximity setter func

--- cl_proximity.lua
+++ cl_proximity.lua
@@ -1,8 +1,14 @@
 DefaultData[#DefaultData + 1] = { "proximity", 1 } -- Add proximity to default player data
 
 function VoiceProperty.proximity(src, data) -- Set voice proximity
-	if config.proximity[data] ~= nil then
+	local proximityMode = config.proximity[data]
+	if proximityMode ~= nil then
 		VoiceData[src].proximity = data
+
+		MumbleSetAudioInputDistance(proximityMode.input + 0.0)
+		MumbleSetAudioOutputDistance(proximityMode.output + 0.0)
+
+		LogMessage(("Change voice proximity to %s (%s) [input: %s, output: %s]"):format(data, proximityMode.name, proximityMode.input, proximityMode.output))
 	end
 end
 
@@ -17,12 +23,7 @@ RegisterCommand("+mumble_proximity", function(src, args, raw)
 			proximity = 1
 		end
 
-		SetVoiceProperty("proximity", src, proximity, true)
-
-		MumbleSetAudioInputDistance(config.proximity[proximity].input + 0.0)
-		MumbleSetAudioOutputDistance(config.proximity[proximity].output + 0.0)
-
-		LogMessage(("Change voice proximity to %s (%s) [input: %s, output: %s]"):format(proximity, config.proximity[proximity].name, config.proximity[proximity].input, config.proximity[proximity].output))
+		SetVoiceProperty("proximity", src, proximity)
 	end
 end)
 
